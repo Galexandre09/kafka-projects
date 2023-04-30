c8a581d4fa07c4c4fb879f5a7a4ac33f1c3fdf6e
Gabriel Alexandre <galexandre2002@gmail.com>
2023-04-30 01:22:47 -0300

    criando e finalizando json-consumer

---
+++ json-consumer/src/main/java/com/gabriel/jsonconsumer/model/Payment.java
@@ -0,0 +1,16 @@
+package com.gabriel.jsonconsumer.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+@ToString
+@Getter
+public class Payment implements Serializable {
+    private long id;
+    private long idUser;
+    private long idProduct;
+    private String cardNumber;
+
+
+}
